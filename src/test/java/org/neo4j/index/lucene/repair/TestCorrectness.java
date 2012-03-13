cf47fd7f6eb9f7f9af1501f22dc4c076900dc566
Chris Gioran <chris.gioran@neotechnology.com>
2012-03-13 22:04:39 +0200

    Added another test and supporting utils

---
+++ src/test/java/org/neo4j/index/lucene/repair/TestCorrectness.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.index.lucene.repair;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.neo4j.index.lucene.repair.util.GraphDatabaseHandler;
+import org.neo4j.test.TargetDirectory;
+
+public class TestCorrectness
+{
+    @Test
+    public void testLeavesValidIndexesIntact() throws Exception
+    {
+        File storeDir = TargetDirectory.forTest( getClass() ).directory( "testLeavesValidIndexesIntact", true );
+        GraphDatabaseHandler db = new GraphDatabaseHandler( storeDir );
+
+        String nodeIndex1 = "node1";
+        db.createNodeIndex( nodeIndex1 );
+        long node1 = db.createAndIndexNode( nodeIndex1, "key1", "value1", false );
+
+        String nodeIndex2 = "node2";
+        db.createNodeIndex( nodeIndex2 );
+        long node2 = db.createAndIndexNode( nodeIndex2, "key2", "value2", false );
+
+        String relationshipIndex = "relationship";
+        db.createRelationshipIndex( relationshipIndex );
+        long relationship = db.createAndIndexRelationship( relationshipIndex, node1, node2, "relType", "key", "value",
+                false );
+
+        db.shutdown();
+        RepairMissingId.main( new String[] { storeDir.getAbsolutePath() } );
+        db.start();
+
+        assertEquals( "missing index value", node1, db.getUniqueFromNodeIndex( nodeIndex1, "key1", "value1" ).getId() );
+        assertEquals( "missing index value", node2, db.getUniqueFromNodeIndex( nodeIndex2, "key2", "value2" ).getId() );
+        assertEquals( "missing index value", relationship,
+                db.getUniqueFromRelationshipIndex( relationshipIndex, "key", "value" ).getId() );
+
+        db.shutdown();
+    }
+}
---
+++ src/test/java/org/neo4j/index/lucene/repair/util/GraphDatabaseHandler.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.index.lucene.repair.util;
+
+import java.io.File;
+
+import org.neo4j.graphdb.DynamicRelationshipType;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+
+public class GraphDatabaseHandler
+{
+    private GraphDatabaseService db;
+    private final File storeDir;
+
+    public GraphDatabaseHandler( File storeDir )
+    {
+        this.storeDir = storeDir;
+        start();
+    }
+
+    public void createNodeIndex( String indexName )
+    {
+        Transaction tx = db.beginTx();
+        try
+        {
+            db.index().forNodes( indexName );
+            tx.success();
+        }
+        catch ( Throwable t )
+        {
+            tx.failure();
+            throw new RuntimeException( t );
+        }
+        finally
+        {
+            tx.finish();
+        }
+    }
+
+    public void createRelationshipIndex( String indexName )
+    {
+        Transaction tx = db.beginTx();
+        try
+        {
+            db.index().forRelationships( indexName );
+            tx.success();
+        }
+        catch ( Throwable t )
+        {
+            tx.failure();
+            throw new RuntimeException( t );
+        }
+        finally
+        {
+            tx.finish();
+        }
+    }
+
+    public long createAndIndexNode( String nodeIndex, String key, Object value, boolean addAsProperty )
+    {
+        Transaction tx = db.beginTx();
+        try
+        {
+            Node n = db.createNode();
+            db.index().forNodes( nodeIndex ).add( n, key, value );
+            if ( addAsProperty )
+            {
+                n.setProperty( key, value );
+            }
+            tx.success();
+            return n.getId();
+        }
+        catch ( Throwable t )
+        {
+            tx.failure();
+            throw new RuntimeException( t );
+        }
+        finally
+        {
+            tx.finish();
+        }
+    }
+
+    public long createAndIndexRelationship( String relationshipIndex, long nodeFrom, long nodeTo,
+            String relType, String key, Object value, boolean addAsProperty )
+    {
+        Transaction tx = db.beginTx();
+        try
+        {
+            Node from = db.getNodeById( nodeFrom );
+            Node to = db.getNodeById( nodeTo );
+            Relationship result = from.createRelationshipTo( to, DynamicRelationshipType.withName( relType ) );
+            db.index().forRelationships( relationshipIndex ).add( result, key, value );
+            if ( addAsProperty )
+            {
+                result.setProperty( key, value );
+            }
+            tx.success();
+            return result.getId();
+        }
+        catch ( Throwable t )
+        {
+            tx.failure();
+            throw new RuntimeException( t );
+        }
+        finally
+        {
+            tx.finish();
+        }
+    }
+
+    public Node getUniqueFromNodeIndex( String nodeIndex, String key, String value )
+    {
+        return db.index().forNodes( nodeIndex ).get( key, value ).getSingle();
+    }
+
+    public Relationship getUniqueFromRelationshipIndex( String relationshipIndex, String key, String value )
+    {
+        return db.index().forRelationships( relationshipIndex ).get( key, value ).getSingle();
+    }
+
+    public void shutdown()
+    {
+        db.shutdown();
+    }
+
+    public void start()
+    {
+        db = new EmbeddedGraphDatabase( storeDir.getAbsolutePath() );
+    }
+
+    public void restart()
+    {
+        shutdown();
+        start();
+    }
+}
